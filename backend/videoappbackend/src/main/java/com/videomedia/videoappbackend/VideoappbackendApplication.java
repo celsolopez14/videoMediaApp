1170dd1bd2e83ad1fc03d59fc1c8d3dc342a6a20
celsolopez14 <celopez@my.bridgeport.edu>
2023-12-17 20:47:59 -0500

    Init backend Spring Boot app

---
+++ backend/videoappbackend/src/main/java/com/videomedia/videoappbackend/VideoappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.videomedia.videoappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoappbackendApplication.class, args);
+	}
+
+}
---
+++ backend/videoappbackend/src/test/java/com/videomedia/videoappbackend/VideoappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videomedia.videoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoappbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
